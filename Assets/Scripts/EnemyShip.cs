84e250e9c008139cad5eada65db1fe48e48004ba
jhong-0 <johon@seas.upenn.edu>
2025-10-04 15:55:41 -0400

    update 10-04-25; bug with turn-based tracker

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -2,31 +2,49 @@ using UnityEngine;
 
 public class EnemyShip : MonoBehaviour
 {
-
     public int hp = 500;
+
+    public PlayerShip player;
+    public int turnFlag = 0;
+
+    public int cannonDamage = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //turnFlag = 0; // Game starts as the player's turn
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (turnFlag == 1 && hp > 0) {
+            Debug.Log("Enemy shoots!\n");
+
+            // Enemy hit rate determined by RNG
+            // TODO: May incorporate formula with player's speed stat
+            // Higher speed = lower chance to get hit by the enemy ship
+            int diceRoll = Random.Range(1, 21);
+            if (diceRoll > 10) {
+                player.TakeHit();
+            }
+
+            turnFlag = 0;
+        }
     }
 
     // Method to be called when the enemy gets hit
     public void TakeHit()
     {
-        hp -= 100;
+        hp -= player.cannonDamage;
         Debug.Log("Enemy hit! Remaining health: " + hp);
 
         if (hp <= 0)
         {
-            Debug.Log("Enemy destroyed!");
+            Debug.Log("Enemy destroyed. They will soon be forgotten, swallowed by the abyss.");
             Destroy(gameObject); // Remove enemy from the scene
+        } else {
+            turnFlag = 1;
         }
     }
 }
--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -2,32 +2,59 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+    public int hp = 500;
 
     public EnemyShip enemy;
+    public int turnFlag = 1;
+
+    public int cannonDamage = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //turnFlag = 1; // Game starts as the player's turn
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the Space key is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Log to the console
-            Debug.Log("Player shoots!\n");
+        //Debug.Log("Player turn flag: " + turnFlag);
+        if (turnFlag == 1 && hp > 0) {
+            // Check if the Space key is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Log to the console
+                Debug.Log("Player shoots!\n");
 
-            if (enemy != null)
-            {
-                enemy.TakeHit();
+                if (enemy != null)
+                {
+                    enemy.TakeHit();
+                    turnFlag = 0;
+                }
+                else
+                {
+                    Debug.LogWarning("Enemy reference not assigned in PlayerShooter!");
+                }
             }
-            else
-            {
-                Debug.LogWarning("Enemy reference not assigned in PlayerShooter!");
-            }
+        }
+    }
+
+    public void TakeHit() {
+        hp -= enemy.cannonDamage;
+        Debug.Log("You suffered a hit to your hull! You have " + hp + " remaining.");
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject); // Remove player from the scene
+            Debug.Log(
+                "You have fallen to a technological force greater than yours. " +
+                "You listen to the cacophony of twisting metal and shattering glass, " +
+                "shrieking into an indifferent and eternal cosmic night." +
+                "And then, the finality of silence.\n\n\n" +
+                "CONTINUE THE CYCLE?"
+            ); // TODO: Prompt for user input to restart the game
+        } else {
+            turnFlag = 1;
         }
     }
 }
